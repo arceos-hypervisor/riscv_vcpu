6f7a8853728db686ddfaf15273216849f91ab015
aarkegz <aarkegz@gmail.com>
2026-03-06 11:19:12 +0800

    remove unused import

--- src/percpu.rs
+++ src/percpu.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use core::marker::PhantomData;
-
 use axerrno::{AxError, AxResult};
 use axvcpu::AxArchPerCpu;
 
